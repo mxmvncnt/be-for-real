b408d9335106785764d6e2cdd9748df8fad17591
Maxime Vincent <maximev@perion.com>
2026-05-30 14:18:57 -0400

    add new tables

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -20,6 +20,14 @@ export const videosTable = pgTable("videos", {
     id: uuid().primaryKey().defaultRandom(),
     userId: uuid().notNull().references(() => usersTable.id, { onDelete: "cascade" }),
     createdAt: timestamp().notNull(),
-    videoUrl: varchar({ length: 1024 }).notNull(),
+    videoUrl: varchar({ length: 1024 }).notNull().unique(),
+    filename: varchar({ length: 1024 }).notNull().unique(),
     type: text({ enum: ["clip", "mashup"] })
 });
+
+export const commentsTable = pgTable("comments", {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid().notNull().references(() => usersTable.id),
+    videoId: uuid().notNull().references(() => videosTable.id),
+    content: varchar({ length: 8096 }).notNull().unique(),
+})
