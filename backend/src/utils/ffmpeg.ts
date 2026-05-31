fce20a438775d754e7f319fd8e0a67d390930070
Maxime Vincent <maximev@perion.com>
2026-05-31 10:00:21 -0400

    add ffmpeg clip

--- backend/src/utils/ffmpeg.ts
+++ backend/src/utils/ffmpeg.ts
@@ -15,3 +15,40 @@ export function concatVideos(listPath: string, outputPath: string): Promise<void
 			.run()
 	})
 }
+
+export function stackTwo(inputs: [string, string], outputPath: string): Promise<void> {
+	return stackVideos(inputs, outputPath, 960)
+}
+
+export function stackThree(inputs: [string, string, string], outputPath: string): Promise<void> {
+	return stackVideos(inputs, outputPath, 640)
+}
+
+export function stackFour(
+	inputs: [string, string, string, string],
+	outputPath: string,
+): Promise<void> {
+	return stackVideos(inputs, outputPath, 480)
+}
+
+function stackVideos(inputs: string[], outputPath: string, rowHeight: number): Promise<void> {
+	return new Promise((resolve) => {
+		const filter = [
+			...inputs.map(
+				(_, i) =>
+					`[${i}:v]scale=1080:${rowHeight}:force_original_aspect_ratio=increase,` +
+					`crop=1080:${rowHeight}[r${i}]`,
+			),
+			`${inputs.map((_, i) => `[r${i}]`).join('')}vstack=inputs=${inputs.length}[v]`,
+		]
+
+		inputs
+			.reduce((cmd, input) => cmd.input(input), ffmpeg())
+			.complexFilter(filter, 'v')
+			.outputOptions(['-c:v libx264', '-pix_fmt yuv420p'])
+			.noAudio()
+			.output(outputPath)
+			.on('end', () => resolve())
+			.run()
+	})
+}
